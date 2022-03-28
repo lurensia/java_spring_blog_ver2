430cb4a3d61f9d566e082acabd55b15dd2a61c2e
Lusia <woder318@gmail.com>
2022-03-28 09:18:41 +0900

    회원정보 수정 완료 - 더티체킹

--- src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
@@ -1,10 +1,12 @@
 package site.metacoding.blogv2.web.api;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import site.metacoding.blogv2.service.UserService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 import site.metacoding.blogv2.web.api.dto.user.LoginDto;
+import site.metacoding.blogv2.web.api.dto.user.UpdateDto;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,21 +24,46 @@ public class UserApiController {
     private final UserService userService;
     private final HttpSession session;
 
+    // password, email, addr
+    @PutMapping("/s/api/user/{id}")
+    public ResponseDto<?> update(@PathVariable Integer id, @RequestBody UpdateDto updateDto) {
+        userService.회원수정(id, updateDto);
+        return new ResponseDto<>(1, "성공", null);
+    }
+
+    // 우리 웹브라우저에서는 현재 사용안함. 추후 앱에서 요청시에 사용할 예정
+    @GetMapping("/s/api/user/{id}")
+    public ResponseDto<?> userInfo(@PathVariable Integer id) {
+        User userEntity = userService.회원정보(id);
+        return new ResponseDto<>(1, "성공", userEntity);
+    }
+
+    // 회원가입 페이지주세요, 회원가입할꼐요, 로그인페이지주세요, 로그인할께요
+    // 로그아웃할께요
+    @GetMapping("/logout")
+    public ResponseDto<?> logout() {
+        session.invalidate();
+        return new ResponseDto<>(1, "성공", null);
+    }
+
     @PostMapping("/join")
-    public ResponseDto<String> join(@RequestBody JoinDto joinDto) {
+    public ResponseDto<?> join(@RequestBody JoinDto joinDto) {
         userService.회원가입(joinDto);
-        return new ResponseDto<String>(1, "회원가입성공", null);
+        return new ResponseDto<>(1, "회원가입성공", null);
     }
 
     @PostMapping("/login")
-    public ResponseDto<String> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
+    public ResponseDto<?> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         User userEntity = userService.로그인(loginDto);
 
         if (userEntity == null) {
-            return new ResponseDto<String>(-1, "로그인실패", null);
+            return new ResponseDto<>(-1, "로그인실패", null);
         }
 
-        response.addHeader("Set-Cookie", "remember=" + loginDto.getUsername() + "; path=/");
+        if (loginDto.getRemember().equals("on")) {
+            response.addHeader("Set-Cookie", "remember=" + loginDto.getUsername() + "; path=/");
+        }
+
         // response.addHeader("Set-Cookie", "remember=" + loginDto.getUsername() + ";
         // path=/; httpOnly=true");
         // response.addHeader("Set-Cookie", "path=/");
