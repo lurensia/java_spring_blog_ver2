858376162f5abc0c83e6a096fd32ff2bba3f687f
Lusia <woder318@gmail.com>
2022-03-28 09:14:24 +0900

    회원정보보기 완료-throw처리

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -1,7 +1,10 @@
 package site.metacoding.blogv2.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.domain.user.UserRepository;
@@ -13,9 +16,20 @@ import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 public class UserService {
     private final UserRepository userRepository;
 
+    public User 회원정보(Integer id) {
+        Optional<User> userOp = userRepository.findById(id);
+
+        if (userOp.isPresent()) {
+            return userOp.get();
+        } else {
+            throw new RuntimeException("아이디를 찾을 수 없습니다.");
+        }
+    }
+
     @Transactional
     public void 회원가입(JoinDto joinDto) {
         // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
+
         userRepository.save(joinDto.toEntity());
 
     }
--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,12 +1,14 @@
 package site.metacoding.blogv2.web;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 
 @RequiredArgsConstructor
@@ -15,16 +17,20 @@ public class UserController {
     private final UserService userService;
     private final HttpSession session;
 
+    // 앱은 이 친구 요청 할까? 아니!!
+    // 웹은 이 친구 요청해야함.
     @GetMapping("/s/user/{id}")
-    public String userInfo() {
+    public String userInfo(@PathVariable Integer id, Model model) {
+        User userEntity = userService.회원정보(id);
+        model.addAttribute("user", userEntity);
         return "user/updateForm";
     }
 
-    @GetMapping("/logout")
-    public String logout() {
-        session.invalidate();// 세션 무효화(세션 아이디 영역의 데이터를 다 삭제함)
-        return "redirect:/";
-    }
+    // @GetMapping("/logout")
+    // public String logout() {
+    // session.invalidate(); // 세션 무효화 (세션 아이디 영역의 데이터를 다 삭제해)
+    // return "redirect:/";
+    // }
 
     // 웹브라우저 -> 회원가입 페이지 주세요!!
     // 앱 -> 회원가입 페이지 주세요!! 말이 안됨!!
@@ -37,5 +43,4 @@ public class UserController {
     public String loginForm() {
         return "user/loginForm";
     }
-
 }
